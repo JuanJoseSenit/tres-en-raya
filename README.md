5c84e32f9743a98f39d94ca7fba54c4e1f53e88c
JuanJoseSenit <juanjo.senit@gmail.com>
2020-08-18 13:20:21 +0200

    juego subir

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# tres-en-raya
+# TRES EN RAYA
+
+El clásico juego de las tres en raya. Dos jugadores (aspa o círculo) se enfrentan por ser el primero en hacer tres en raya. Cada jugador dispone de tres fichas (aspas o círculos). En las 6 primeras rondas van introduciendo, por turnos, una ficha cada jugador. Acabados estos turnos, van moviendo las fichas situadas en el tablero, por turnos, hasta hacer las tres en raya.
+___
+## DESARROLLO DE UNA PARTIDA
+![img1](./assets/imgmd/img1.png)
+En la primera imagen se muestra el tablero inicial, con el jugador que empieza
+
+![img2](./assets/imgmd/img2.png)
+En la segunda imagen, cada jugador ha introducido sus tres fichas (por turnos) y al no conseguir ninguno de ellos hacer tres en raya, comienzan a mover una pieza cada uno por turnos hasta que alguno de ellos gane
+
+![img2](./assets/imgmd/img3.png)
+En la tercera se observa el final de una partida, en la cual ha ganado el jugador círculo, ya que ha conseguido realizar las 3 en raya, marcándose el resultado ganador
+___
+## CÓMO JUGAR
+Existen varias alternativas para ejecutar el juego:
+1. Clonar el repositorio y **ejecutar** el **index.html**
+2. Aprovechando la herramienta de GitHub Pages desde el siguiente enlace:
--- juego.js
+++ juego.js
@@ -1,4 +1,4 @@
-let arrayColumnas = [
+let arrayFilas = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""],
@@ -42,7 +42,7 @@ function quienEmpieza() {
   }
 }
 function meterFicha(e) {
-  let quienVa=document.getElementById("ronda_jugador");
+  let quienVa = document.getElementById("ronda_jugador");
   let arrayColFila = e.target.id.split("-");
   let fila = arrayColFila[0];
   let columna = arrayColFila[1];
@@ -50,25 +50,25 @@ function meterFicha(e) {
   imagen.setAttribute("class", "imagenesFichas");
   imagen.setAttribute("id", fila + "." + columna);
   //console.log(fila, columna);
-  if (!arrayColumnas[fila][columna] == "") {
+  if (!arrayFilas[fila][columna] == "") {
     console.log("Ya está ocupada");
   } else if (jugadorCirculo == true) {
-    arrayColumnas[fila][columna] = "circulo";
+    arrayFilas[fila][columna] = "circulo";
     imagen.setAttribute("src", "./assets/img/circulo.png");
     console.log(e.target);
     e.target.appendChild(imagen);
     fichasCirculo--;
     jugadorCirculo = false;
-    quienVa.innerHTML= "JUGADOR X"
+    quienVa.innerHTML = "JUGADOR X"
   } else {
-    arrayColumnas[fila][columna] = "aspa";
+    arrayFilas[fila][columna] = "aspa";
     imagen.setAttribute("src", "./assets/img/aspa.png");
     e.target.appendChild(imagen);
     fichasAspa--;
     jugadorCirculo = true;
-    quienVa.innerHTML="JUGADOR CÍRCULO"
+    quienVa.innerHTML = "JUGADOR CÍRCULO"
   }
-  console.log(arrayColumnas);
+  console.log(arrayFilas);
   if (fichasAspa <= 0 && fichasCirculo <= 0) {
     let arraytd = document.getElementsByClassName("tdTablero");
     for (let i = 0; i < arraytd.length; i++) {
@@ -86,83 +86,122 @@ function meterFicha(e) {
   comprobarVertical(columna);
   comprobarDiagonal(fila, columna);
 }
-function darGanador(contadorCirculo,contadorAspa){
-  let quienVa=document.getElementById("ronda_jugador");
+function darGanador(contadorCirculo, contadorAspa, resaltarCirculo = [], resaltarAspa = []) {
+  let quienVa = document.getElementById("ronda_jugador");
   if (contadorCirculo == 3) {
-    quienVa.innerHTML="GANA EL JUGADOR CÍRCULO"
+    quienVa.innerHTML = "GANA EL JUGADOR CÍRCULO"
   } else if (contadorAspa == 3) {
-    quienVa.innerHTML="GANA EL JUGADOR ASPA"
+    quienVa.innerHTML = "GANA EL JUGADOR ASPA"
   }
-  if(contadorCirculo==3 || contadorAspa==3){
-    let tablero=document.getElementsByClassName("tdTablero")
-    for(let i=0;i<tablero.length;i++){
+  if (contadorCirculo == 3 || contadorAspa == 3) {
+    let tablero = document.getElementsByClassName("tdTablero")
+    for (let i = 0; i < tablero.length; i++) {
       tablero[i].removeAttribute("onclick")
       tablero[i].removeAttribute("ondrop")
     }
-  } 
+  }
+  if (resaltarCirculo.length == 3) {
+    console.log("Hay que remarcar las casillas ganadoras")
+    for (let i = 0; i < resaltarCirculo.length; i++) {
+      document.getElementById(resaltarCirculo[i]).style.backgroundColor = "#28bfd3";
+    }
+  } else if (resaltarAspa.length == 3) {
+    console.log("Hay que remarcar las casillas ganadoras")
+    for (let i = 0; i < resaltarAspa.length; i++) {
+      document.getElementById(resaltarAspa[i]).style.backgroundColor = "#28bfd3";
+    }
+  }
 }
 function comprobarHorizontal(fila) {
   let contadorCirculo = 0;
   let contadorAspa = 0;
-  for (let i = 0; i < arrayColumnas[fila].length; i++) {
-    if (arrayColumnas[fila][i] == "circulo") {
+  let arrayPosicionResaltarCirculo = [];
+  let arrayPosicionResaltarAspa = [];
+  for (let i = 0; i < arrayFilas[fila].length; i++) {
+    if (arrayFilas[fila][i] == "circulo") {
+      arrayPosicionResaltarCirculo.push(fila + "-" + i)
       contadorCirculo++;
-    } else if (arrayColumnas[fila][i] == "aspa") {
+    } else if (arrayFilas[fila][i] == "aspa") {
+      arrayPosicionResaltarAspa.push(fila + "-" + i)
       contadorAspa++;
     }
   }
-  darGanador(contadorCirculo,contadorAspa)
-  
+  darGanador(contadorCirculo, contadorAspa, arrayPosicionResaltarCirculo, arrayPosicionResaltarAspa)
+
 }
 function comprobarVertical(columna) {
   let contadorCirculo = 0;
   let contadorAspa = 0;
-  for (let i = 0; i < arrayColumnas.length; i++) {
-    if (arrayColumnas[i][columna] == "circulo") {
+  let arrayPosicionResaltarCirculo = [];
+  let arrayPosicionResaltarAspa = [];
+  for (let i = 0; i < arrayFilas.length; i++) {
+    if (arrayFilas[i][columna] == "circulo") {
+      arrayPosicionResaltarCirculo.push(i + "-" + columna)
       contadorCirculo++;
-    } else if (arrayColumnas[i][columna] == "aspa") {
+    } else if (arrayFilas[i][columna] == "aspa") {
+      arrayPosicionResaltarAspa.push(i + "-" + columna)
       contadorAspa++;
     }
   }
-  darGanador(contadorCirculo,contadorAspa)
+  darGanador(contadorCirculo, contadorAspa, arrayPosicionResaltarCirculo, arrayPosicionResaltarAspa)
 }
 function comprobarDiagonal(fila, columna) {
   //Diagonal pte negativa
   if (fila == columna) {
     console.log("Se debe comprobar la diag pte negativa");
     if (
-      arrayColumnas[0][0] == "circulo" &&
-      arrayColumnas[1][1] == "circulo" &&
-      arrayColumnas[2][2] == "circulo"
+      arrayFilas[0][0] == "circulo" &&
+      arrayFilas[1][1] == "circulo" &&
+      arrayFilas[2][2] == "circulo"
     ) {
-      darGanador(3,0)
+      darGanador(3, 0,["0-0","1-1","2-2"],[])
     }
     if (
-      arrayColumnas[0][0] == "aspa" &&
-      arrayColumnas[1][1] == "aspa" &&
-      arrayColumnas[2][2] == "aspa"
+      arrayFilas[0][0] == "aspa" &&
+      arrayFilas[1][1] == "aspa" &&
+      arrayFilas[2][2] == "aspa"
     ) {
-      darGanador(0,3)
+      darGanador(0, 3,[],["0-0","1-1","2-2"])
     }
   }
   //Diagonal pte positiva
-   let contadorCirculo=0;
-  let contadorAspa=0;
-  if(arrayColumnas[fila][columna]=="circulo") contadorCirculo++;
-  else if(arrayColumnas[fila][columna]=="aspa") contadorAspa++;
-  console.log(contadorAspa,contadorCirculo)
-  for(let i=1;i<arrayColumnas.length;i++){
-    console.log(parseInt(columna)+i)
-    if(fila-i>=0 && parseInt(columna)+i<=2){
-      if(arrayColumnas[fila-i][parseInt(columna)+i]=="circulo") contadorCirculo++;
-      else if(arrayColumnas[fila-i][parseInt(columna)+i]=="aspa") contadorAspa++;
-    }
-     if(parseInt(fila)+i<=2 && columna-i>=0){
-      if(arrayColumnas[parseInt(fila)+i][columna-i]=="circulo") contadorCirculo++;
-      else if(arrayColumnas[parseInt(fila)+i][columna-i]=="aspa") contadorAspa++;
-    } 
+  let contadorCirculo = 0;
+  let contadorAspa = 0;
+  let arrayPosicionResaltarCirculo = [];
+  let arrayPosicionResaltarAspa = [];
+  if (arrayFilas[fila][columna] == "circulo"){
+    arrayPosicionResaltarCirculo.push(fila + "-" + columna)
+    contadorCirculo++
   }
-  darGanador(contadorCirculo,contadorAspa) 
+  else if (arrayFilas[fila][columna] == "aspa"){
+    arrayPosicionResaltarAspa.push(fila + "-" + columna)
+    contadorAspa++
+  }
+  console.log(contadorAspa, contadorCirculo)
+  for (let i = 1; i < arrayFilas.length; i++) {
+    console.log(parseInt(columna) + i)
+    if (fila - i >= 0 && parseInt(columna) + i <= 2) {
+      if (arrayFilas[fila - i][parseInt(columna) + i] == "circulo"){
+        arrayPosicionResaltarCirculo.push(fila - i + "-" + (parseInt(columna) + i))
+        contadorCirculo++
+      }
+      else if (arrayFilas[fila - i][parseInt(columna) + i] == "aspa"){
+        arrayPosicionResaltarAspa.push(fila - i + "-" + (parseInt(columna) + i))
+        contadorAspa++}
+    }
+    if (parseInt(fila) + i <= 2 && columna - i >= 0) {
+      if (arrayFilas[parseInt(fila) + i][columna - i] == "circulo"){
+        arrayPosicionResaltarCirculo.push((parseInt(fila) + i) + "-" + columna - i)
+        contadorCirculo++}
+      else if (arrayFilas[parseInt(fila) + i][columna - i] == "aspa"){
+        arrayPosicionResaltarAspa.push((parseInt(fila) + i) + "-" + columna - i)
+        contadorAspa++
+      }
+    }
+  }
+  console.log("Diagonal pte positiva cirulo",arrayPosicionResaltarCirculo)
+  console.log("Diagonal pte positiva aspa",arrayPosicionResaltarAspa)
+  darGanador(contadorCirculo, contadorAspa,arrayPosicionResaltarCirculo,arrayPosicionResaltarAspa)
 
 }
 function arrastrar(e) {
@@ -181,7 +220,7 @@ function permitir(e) {
   e.preventDefault();
 }
 function soltar(e) {
-  let quienVa=document.getElementById("ronda_jugador");
+  let quienVa = document.getElementById("ronda_jugador");
   let imagen = document.createElement("img");
   let src = e.dataTransfer.getData("Text");
   let idImagen = e.dataTransfer.getData("Text2");
@@ -190,39 +229,39 @@ function soltar(e) {
   let fila = arrayColFila[0];
   let columna = arrayColFila[1];
   console.log(idtd);
-  if(idImagen!=""){
-  if (arrayColumnas[fila][columna] == "") {
-    e.target.appendChild(imagen);
-    imagen.setAttribute("src", src);
-    imagen.setAttribute("id", fila + "." + columna);
-    imagen.setAttribute("draggable", "true");
-    imagen.setAttribute("ondragstart", "arrastrar(event)");
-    document
-      .getElementById(idtd)
-      .removeChild(document.getElementById(idtd).firstChild);
+  if (idImagen != "") {
+    if (arrayFilas[fila][columna] == "") {
+      e.target.appendChild(imagen);
+      imagen.setAttribute("src", src);
+      imagen.setAttribute("id", fila + "." + columna);
+      imagen.setAttribute("draggable", "true");
+      imagen.setAttribute("ondragstart", "arrastrar(event)");
+      document
+        .getElementById(idtd)
+        .removeChild(document.getElementById(idtd).firstChild);
 
-    let arrayidtd = idtd.split("-");
-    arrayColumnas[arrayidtd[0]][arrayidtd[1]] = "";
-    if (src == "./assets/img/circulo.png") {
-      arrayColumnas[fila][columna] = "circulo";
-      jugadorCirculo = false;
-      quienVa.innerHTML= "JUGADOR X"
-    } else if (src == "./assets/img/aspa.png") {
-      arrayColumnas[fila][columna] = "aspa";
-      jugadorCirculo = true;
-      quienVa.innerHTML= "JUGADOR CÍRCULO"
+      let arrayidtd = idtd.split("-");
+      arrayFilas[arrayidtd[0]][arrayidtd[1]] = "";
+      if (src == "./assets/img/circulo.png") {
+        arrayFilas[fila][columna] = "circulo";
+        jugadorCirculo = false;
+        quienVa.innerHTML = "JUGADOR X"
+      } else if (src == "./assets/img/aspa.png") {
+        arrayFilas[fila][columna] = "aspa";
+        jugadorCirculo = true;
+        quienVa.innerHTML = "JUGADOR CÍRCULO"
+      }
+    } else {
+      console.log("ocupado");
     }
-  } else {
-    console.log("ocupado");
-  }
-  comprobarHorizontal(fila);
-  comprobarVertical(columna);
-  comprobarDiagonal(fila, columna);
+    comprobarHorizontal(fila);
+    comprobarVertical(columna);
+    comprobarDiagonal(fila, columna);
 
-  //console.log("jugadorCirculo?", jugadorCirculo);
-  //console.log("soltar", e.target);
-  //console.log("src", src);
-  console.log(arrayColumnas);
+    //console.log("jugadorCirculo?", jugadorCirculo);
+    //console.log("soltar", e.target);
+    //console.log("src", src);
+    console.log(arrayFilas);
   }
 }
 
